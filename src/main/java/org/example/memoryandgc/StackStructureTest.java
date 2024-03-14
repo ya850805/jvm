f3bfa28e565051d122a21d53cc0fb4ca212cb07f
Jason <ya850805@gmail.com>
2024-03-14 18:31:41 +0800

    docs: Add JVM Structure doc.

---
+++ src/main/java/org/example/memoryandgc/StackStructureTest.java
@@ -0,0 +1,18 @@
+package org.example.memoryandgc;
+
+/**
+ * @author jason
+ * @description
+ * @create 2024/3/14 18:02
+ *
+ *  javap -v StackStructureTest
+ **/
+public class StackStructureTest {
+    public static void main(String[] args) {
+//        int i = 2 + 3;
+
+        int i = 2;
+        int j = 3;
+        int k = i + j;
+    }
+}
