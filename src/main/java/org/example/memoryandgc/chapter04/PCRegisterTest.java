4d3fda6d5e05d6d8e69d67eae34a43284ef4981d
Jason <ya850805@gmail.com>
2024-03-20 15:47:13 +0800

    docs: Add PC Register intro.

---
+++ src/main/java/org/example/memoryandgc/chapter04/PCRegisterTest.java
@@ -0,0 +1,18 @@
+package org.example.memoryandgc.chapter04;
+
+/**
+ * @author jason
+ * @description
+ * @create 2024/3/20 15:11
+ **/
+public class PCRegisterTest {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+
+        String s = "abc";
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
