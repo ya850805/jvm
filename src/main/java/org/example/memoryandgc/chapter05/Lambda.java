351f96352fe65d65942a1ba6fa6278f11cc920a1
Jason <ya850805@gmail.com>
2024-03-27 18:37:59 +0800

    docs: Add invokedynamic & virtual method table & return address intro.

---
+++ src/main/java/org/example/memoryandgc/chapter05/Lambda.java
@@ -0,0 +1,30 @@
+package org.example.memoryandgc.chapter05;
+
+/**
+ * 體會invokedynamic
+ */
+
+@FunctionalInterface
+interface Func {
+    public boolean func(String str);
+}
+
+public class Lambda {
+    public void lambda(Func func) {
+        return;
+    }
+
+    public static void main(String[] args) {
+        Lambda lambda = new Lambda();
+
+        Func func = s -> {
+            return true;
+        };
+
+        lambda.lambda(func);
+
+        lambda.lambda(s -> {
+            return true;
+        });
+    }
+}
